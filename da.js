0b0c05b03e6257571b27e54744a4a47d7ee80369
as0n <as0n@gnab.fr>
2015-05-24 01:08:29 +0200

    First verion.

---
+++ da.js
@@ -0,0 +1,50 @@
+var request = require('request');
+
+function DA(client_id, client_secret) {
+	this.client_id = client_id;
+	this.client_secret = client_secret;
+
+	this.dailies = {
+		cache : null,
+		lastRequest : 0
+	}
+}
+DA.cacheExpiry = 60*60*1000; //1h
+
+DA.prototype.grabToken = function() {
+	var da = this;
+
+	request({
+		url: 'https://www.deviantart.com/oauth2/token',
+		qs: {
+			client_id: this.client_id,
+			client_secret: this.client_secret,
+			grant_type: "client_credentials"
+		},
+		json: true
+	}, function(err, res, body) {
+		da.token = body['access_token'];
+		console.log('DA : got API token : %s', da.token);
+	});
+};
+DA.prototype.getDailies = function(cb) {
+	var da = this;
+	if (new Date() - this.dailies.lastRequest <= DA.cacheExpiry) {
+		return cb(false, this.dailies.cache);
+	}
+
+	request({
+		url: 'https://www.deviantart.com/api/v1/oauth2/browse/dailydeviations?mature_content=true',
+		qs: {
+			access_token: this.token
+		},
+		json: true
+	}, function(err, resp, body) {
+		da.dailies.cache = body;
+		da.dailies.lastRequest = new Date();
+		console.log("DA : refreshed dailies.")
+		return cb(err, body);
+	});
+};
+
+module.exports = DA;
---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express'),
+	DA = require('./da');
+
+var app = express(),
+	da;
+
+app.use('/static', express.static('public'));
+
+app.get('/', function(req, res) {
+	//res.set('Access-Control-Allow-Origin', '*');
+	da.getDailies(function(err, data) {
+		var r;
+
+		while (true) {
+			r = Math.floor(Math.random()*data.results.length);
+			if (data.results[r].content) {
+				return res.json({
+					img: data.results[r].content.src,
+					url: data.results[r].url,
+					title: data.results[r].title,
+					author: data.results[r].author.username
+				});
+			}
+		}
+	});
+});
+
+if (process.argv.length > 3) {
+	da = new DA(process.argv[2], process.argv[3]);
+	da.grabToken();
+
+	var server = app.listen(3000, function() {
+		var host = server.address().address,
+			port = server.address().port;
+
+		// We're ready
+		console.log('40XDA listening on %s:%s', host, port);
+	});
+}
+else {
+	console.log('DA application credentials missing. Exiting.');
+	return;
+}
